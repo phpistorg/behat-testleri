b4eedcd06a803c9f8bca9089d2e5850804f5fe9d
Mustafa Ileri <mi@mustafaileri.com>
2014-05-01 15:38:03 +0300

    First behat test.

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,41 @@
+<?php
+
+use Behat\Behat\Context\BehatContext;
+use Behat\Gherkin\Node\PyStringNode;
+
+class FeatureContext extends BehatContext
+{
+    private $output;
+
+    /** @Given /^I am in a directory "([^"]*)"$/ */
+    public function iAmInADirectory($dir)
+    {
+        if (!file_exists($dir)) {
+            mkdir($dir);
+        }
+        chdir($dir);
+    }
+
+    /** @Given /^I have a file named "([^"]*)"$/ */
+    public function iHaveAFileNamed($file)
+    {
+        touch($file);
+    }
+
+    /** @When /^I run "([^"]*)"$/ */
+    public function iRun($command)
+    {
+        exec($command, $output);
+        $this->output = trim(implode("\n", $output));
+    }
+
+    /** @Then /^I should get:$/ */
+    public function iShouldGet(PyStringNode $string)
+    {
+        if ((string) $string !== $this->output) {
+            throw new Exception(
+                "Actual output is:\n" . $this->output
+            );
+        }
+    }
+}
